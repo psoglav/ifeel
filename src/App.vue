7ca30a17327f8dc6862756a9373d6f987410a954
Andrey Nevinny <psoglav.ih8u@gmail.com>
2021-03-24 21:56:36 +0600

    f-end: temp custom scrollbar

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,24 @@ html {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  
+
   color: $primary-foreground;
+}
+
+*::-webkit-scrollbar {
+  width: 6px;
+}
+
+*::-webkit-scrollbar-track {
+  background: transparent;
+}
+
+*::-webkit-scrollbar-thumb {
+  background: $subtle-light;
+  border-radius: 3px;
+}
+
+*::-webkit-scrollbar-thumb:hover {
+  background: $half-light;
 }
 </style>
